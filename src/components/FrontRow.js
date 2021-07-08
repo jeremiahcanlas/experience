80477d9c5eb5a63e9c9d5d345bfc80feefe5e834
jeremiahcanlas <jeremiahcanlas@gmail.com>
2021-07-07 20:31:38 -0400

    minor fix

--- src/components/FrontRow.js
+++ src/components/FrontRow.js
@@ -1,22 +1,22 @@
-import React, { useEffect } from 'react';
-import video from '../utils/videos/rally.mp4';
-import Button from './button/Button';
-import Fade from 'react-reveal/Fade';
+import React, { useEffect } from "react";
+import video from "../utils/videos/rally.mp4";
+import Button from "./button/Button";
+import Fade from "react-reveal/Fade";
 // import Pulse from 'react-reveal/Pulse';
 // import Slide from 'react-reveal/Slide';
 // import Bounce from 'react-reveal/Bounce';
 
 const FrontRow = ({ component: { compo } }) => {
   useEffect(() => {
-    const vid = document.getElementById('video');
+    const vid = document.getElementById("video");
 
     vid.currentTime = 0;
 
-    compo === 'frontRow' ? vid.play() : vid.pause();
+    compo === "frontRow" ? vid.play() : vid.pause();
   }, [compo]);
 
   return (
-    <div className='frontRow'>
+    <div className="frontRow">
       <video
         // style={
         //   compo === 'frontRow' ? { display: 'visible' } : { display: 'hidden' }
@@ -24,19 +24,19 @@ const FrontRow = ({ component: { compo } }) => {
         muted
         loop
         playsInline
-        id='video'
+        id="video"
       >
-        <source src={video} type='video/mp4' />
+        <source src={video} type="video/mp4" />
       </video>
 
-      <div className='content' style={{ textAlign: 'left' }}>
-        <Fade left duration={1000} delay={500}>
-          <h1 className='fancy'>FRONT ROW SEATS</h1>
+      <div className="content" style={{ textAlign: "left" }}>
+        <Fade left duration={1000} delay={200}>
+          <h1 className="fancy">FRONT ROW SEATS</h1>
         </Fade>
-        <Fade duration={2000} delay={1200}>
+        <Fade duration={2000} delay={500}>
           <p>Experience rally racing up close and personal.</p>
         </Fade>
-        <Button name={'try it now'} duration={1000} delay={1500} />
+        <Button name={"try it now"} duration={1000} delay={1000} />
       </div>
     </div>
   );
--- src/components/Sound.js
+++ src/components/Sound.js
@@ -1,42 +1,36 @@
-import React from 'react';
-import image from '../utils/img/speaker-sound-box-pngrepo-com.png';
-import { Link as Scroll } from 'react-scroll';
-import Fade from 'react-reveal/Fade';
+import React from "react";
+import image from "../utils/img/speaker-sound-box-pngrepo-com.png";
+import { Link as Scroll } from "react-scroll";
+import Fade from "react-reveal/Fade";
 // import Pulse from 'react-reveal/Pulse';
 // import Slide from 'react-reveal/Slide';
 // import Bounce from 'react-reveal/Bounce';
 
 const Sound = () => {
   return (
-    <div className='sound-container'>
-      <div className='superior'>
-        <Fade left duration={1000} delay={700}>
+    <div className="sound-container">
+      <div className="superior">
+        <Fade left duration={1000} delay={300}>
           <h1>EXCEPTIONAL SOUND</h1>
         </Fade>
-        <Fade duration={2000} delay={1500}>
+        <Fade duration={2000} delay={500}>
           <p>
             Experience live versions of your <br /> favourite rally team
           </p>
         </Fade>
       </div>
 
-      <Fade cascade duration={1500} delay={600}>
-        <div className='speakers'>
-          <img style={{ marginBottom: '6rem' }} src={image} alt='png' />
-          <img style={{ marginTop: '6rem' }} src={image} alt='png' />
+      <Fade cascade duration={1500} delay={500}>
+        <div className="speakers">
+          <img style={{ marginBottom: "6rem" }} src={image} alt="png" />
+          <img style={{ marginTop: "6rem" }} src={image} alt="png" />
         </div>
       </Fade>
-      {/* 
-      <Fade top duration={1500} delay={1500}>
-        <div className='scroll'>
-          <p>scroll down for more info</p>
-          <i className='fa fa-angle-down' aria-hidden='true'></i>
-        </div>
-      </Fade> */}
+
       <Fade top duration={1500} delay={600}>
-        <div className='scroll'>
-          <Scroll smooth to='frontRow'>
-            <i className='fa fa-angle-down' aria-hidden='true'></i>
+        <div className="scroll">
+          <Scroll smooth to="frontRow">
+            <i className="fa fa-angle-down" aria-hidden="true"></i>
           </Scroll>
         </div>
       </Fade>
